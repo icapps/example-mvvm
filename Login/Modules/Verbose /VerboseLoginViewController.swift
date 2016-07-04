eeb76dec8dae085a4cc2062302cc9d15ca67e91c
Jelle Vandebeeck <jelle@fousa.be>
2016-07-04 20:54:12 +0200

    Add outlets and actions.

---
+++ Login/Modules/Verbose /VerboseLoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  VerboseLoginViewController.swift
+//  Login
+//
+//  Created by Jelle Vandebeeck on 04/07/16.
+//  Copyright © 2016 iCapps. All rights reserved.
+//
+
+import UIKit
+
+class VerboseLoginViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet var button: UIButton!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    // MARK: - Actions
+    
+    @IBAction func verifyPassword(sender: AnyObject) {
+        
+    }
+    
+}
